068824aa39ee71663851048c6de3aeb117f105e0
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-03-24 12:54:54 +0300

    move room from ArticleDatabase to AppModule

--- app/src/main/java/com/suveybesena/retrofitcoroutines/data/database/ArticleDatabases.kt
+++ app/src/main/java/com/suveybesena/retrofitcoroutines/data/database/ArticleDatabases.kt
@@ -1,36 +1,19 @@
 package com.suveybesena.retrofitcoroutines.data.database
 
-import android.content.Context
+
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.suveybesena.retrofitcoroutines.data.models.Article
+import com.suveybesena.retrofitcoroutines.data.model.Article
 
 @Database(
     entities = [Article::class],
-    version = 2
+    version = 2,
+    exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class ArticleDatabase : RoomDatabase() {
 
-    abstract fun getArticleDao(): ArticleDao
+    abstract fun articleDao(): ArticleDao
 
-    companion object {
-        @Volatile
-        private var instance: ArticleDatabase? = null
-        private val LOCK = Any()
-
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
-            instance ?: createDatabase(context).also { instance = it }
-        }
-
-        private fun createDatabase(context: Context) =
-            Room.databaseBuilder(
-                context.applicationContext,
-                ArticleDatabase::class.java,
-                "article_db.db"
-            ).fallbackToDestructiveMigration().build()
-
-    }
 }
