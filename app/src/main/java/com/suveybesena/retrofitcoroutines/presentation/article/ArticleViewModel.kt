ffca67a8fb863c295de33aeefc28095709c5a889
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-03-24 12:58:27 +0300

    add @HiltViewModel annotation to ArticleViewModel

--- app/src/main/java/com/suveybesena/retrofitcoroutines/presentation/article/ArticleViewModel.kt
+++ app/src/main/java/com/suveybesena/retrofitcoroutines/presentation/article/ArticleViewModel.kt
@@ -2,11 +2,14 @@ package com.suveybesena.retrofitcoroutines.presentation.article
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.suveybesena.retrofitcoroutines.data.models.Article
+import com.suveybesena.retrofitcoroutines.data.model.Article
 import com.suveybesena.retrofitcoroutines.data.repository.NewsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ArticleViewModel (
+@HiltViewModel
+class ArticleViewModel @Inject constructor (
     val newsRepository: NewsRepository
         ) :ViewModel(){
 
