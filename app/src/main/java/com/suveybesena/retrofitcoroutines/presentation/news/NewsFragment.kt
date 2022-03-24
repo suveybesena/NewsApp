fbcd7e6ddd1dcea9ca093258ede71bac9fcd09fd
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-03-24 13:03:52 +0300

    add Hilt and romove ProviderFactory from NewsFragment

--- app/src/main/java/com/suveybesena/retrofitcoroutines/presentation/news/NewsFragment.kt
+++ app/src/main/java/com/suveybesena/retrofitcoroutines/presentation/news/NewsFragment.kt
@@ -5,21 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.suveybesena.retrofitcoroutines.R
 import com.suveybesena.retrofitcoroutines.adapters.NewsAdapter
 import com.suveybesena.retrofitcoroutines.databinding.FragmentNewsBinding
-import com.suveybesena.retrofitcoroutines.presentation.MainActivity
 import com.suveybesena.retrofitcoroutines.common.Resources
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class NewsFragment : Fragment() {
-
-    lateinit var viewModel: NewsViewModel
+    private val viewModel: NewsViewModel by viewModels()
     lateinit var newsAdapter: NewsAdapter
-
     private lateinit var binding: FragmentNewsBinding
 
     override fun onCreateView(
@@ -32,10 +31,8 @@ class NewsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = (activity as MainActivity).newsViewModel
         setupRecyclerView()
         observeData()
-
     }
 
     private fun observeData() {
@@ -57,7 +54,6 @@ class NewsFragment : Fragment() {
         binding.recyclerView.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
-
         }
         newsAdapter.setOnItemClickListener {
             val bundle = Bundle().apply {
@@ -67,8 +63,6 @@ class NewsFragment : Fragment() {
         }
 
     }
-
-
 }
 
 
