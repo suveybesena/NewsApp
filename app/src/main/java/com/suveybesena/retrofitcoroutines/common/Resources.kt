8f3a64ef5519749fde8189caf4a5a91ae95e5080
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-03-24 13:05:37 +0300

    remove comment line

--- app/src/main/java/com/suveybesena/retrofitcoroutines/common/Resources.kt
+++ app/src/main/java/com/suveybesena/retrofitcoroutines/common/Resources.kt
@@ -4,11 +4,10 @@ sealed class Resources<T>(
     val data: T? = null,
     val message: String? = null,
 ) {
-
     class success<T>(data: T) : Resources<T>(data)
     class error<T>(message: String, data: T? = null) : Resources<T>(data, message)
     class loading<T> : Resources<T>()
-    //object loading  : Resources<Nothing>()
+
 
 
 }
