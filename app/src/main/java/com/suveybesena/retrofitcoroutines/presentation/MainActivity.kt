98e1ebe8c8c4be5be152fe5abd2e0e6361ccdab3
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-03-24 13:01:58 +0300

    add Hilt and remove ViewModelProviderFactory classes from MainActivity

--- app/src/main/java/com/suveybesena/retrofitcoroutines/presentation/MainActivity.kt
+++ app/src/main/java/com/suveybesena/retrofitcoroutines/presentation/MainActivity.kt
@@ -2,7 +2,6 @@ package com.suveybesena.retrofitcoroutines.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,45 +9,14 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.suveybesena.retrofitcoroutines.R
-import com.suveybesena.retrofitcoroutines.presentation.savednews.SavedNewsModelProviderFactory
-import com.suveybesena.retrofitcoroutines.data.database.ArticleDatabase
-import com.suveybesena.retrofitcoroutines.data.repository.NewsRepository
-import com.suveybesena.retrofitcoroutines.presentation.article.ArticleViewModel
-import com.suveybesena.retrofitcoroutines.presentation.article.ArticleViewModelProviderFactory
-import com.suveybesena.retrofitcoroutines.presentation.news.NewsViewModel
-import com.suveybesena.retrofitcoroutines.presentation.news.NewsViewModelProviderFactory
-import com.suveybesena.retrofitcoroutines.presentation.savednews.SavedNewsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    lateinit var newsViewModel: NewsViewModel
-    lateinit var savedNewsViewModel: SavedNewsViewModel
-    lateinit var articleViewModel : ArticleViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val newsRepository = NewsRepository(ArticleDatabase(this))
-
-        val newsviewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
-        newsViewModel =
-            ViewModelProvider(this, newsviewModelProviderFactory).get(NewsViewModel::class.java)
-
-        val savedNewsModelProviderFactory = SavedNewsModelProviderFactory(newsRepository)
-        savedNewsViewModel = ViewModelProvider(
-            this,
-            savedNewsModelProviderFactory
-        ).get(SavedNewsViewModel::class.java)
-
-        val articleViewModelProviderFactory = ArticleViewModelProviderFactory(newsRepository)
-        articleViewModel = ViewModelProvider(
-            this,
-            articleViewModelProviderFactory
-        ).get(ArticleViewModel::class.java)
-
-
 
         this.supportActionBar?.hide()
 
