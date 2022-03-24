403f0a62c0661c33d0d62f8879908cdc36ab2ee8
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-03-24 13:07:13 +0300

    add Hilt to SavedNewsViewModel

--- app/src/main/java/com/suveybesena/retrofitcoroutines/presentation/savednews/SavedNewsViewModel.kt
+++ app/src/main/java/com/suveybesena/retrofitcoroutines/presentation/savednews/SavedNewsViewModel.kt
@@ -2,30 +2,23 @@ package com.suveybesena.retrofitcoroutines.presentation.savednews
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.suveybesena.retrofitcoroutines.data.models.Article
+import com.suveybesena.retrofitcoroutines.data.model.Article
 import com.suveybesena.retrofitcoroutines.data.repository.NewsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SavedNewsViewModel(val newsRepository: NewsRepository) :ViewModel() {
-
-    //val getSavedNews: MutableLiveData<List<Article>> = MutableLiveData()
+@HiltViewModel
+class SavedNewsViewModel @Inject constructor(val newsRepository: NewsRepository) : ViewModel() {
 
     fun saveArticle(article: Article) = viewModelScope.launch {
         newsRepository.upsert(article)
     }
-
     fun getSavedNews() = newsRepository.getSavedNews()
-
-    //fun getSavedNews () = viewModelScope.launch {
-    //   newsRepository.getSavedNews().collect {
-    //       getSavedNews.value = it
-    //   }
-    //}
 
     fun deleteArticle(article: Article) = viewModelScope.launch {
         newsRepository.deleteArticle(article)
     }
 
 
-  
 }
