db07701a0245e2353f11b0aec3d82366337ec56e
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-03-24 12:51:08 +0300

    add Serializable to data class

--- app/src/main/java/com/suveybesena/retrofitcoroutines/data/model/Article.kt
+++ app/src/main/java/com/suveybesena/retrofitcoroutines/data/model/Article.kt
@@ -4,7 +4,6 @@ package com.suveybesena.retrofitcoroutines.data.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 
