e184e3cf6432e4687fe5b2bd8e81d0ba2ec8e3ad
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-03-24 12:59:07 +0300

    remove BASE_URL from Constants

--- app/src/main/java/com/suveybesena/retrofitcoroutines/common/Constants.kt
+++ app/src/main/java/com/suveybesena/retrofitcoroutines/common/Constants.kt
@@ -3,7 +3,6 @@ package com.suveybesena.retrofitcoroutines.common
 class Constants {
     companion object {
         const val API_KEY = "99a613e2ef744c5482de7657df4adfea"
-        const val BASE_URL = "https://newsapi.org"
     }
 
 }
